735f8980ee9cc7a83080b43b83f2d82cb9b98033
Izak <izakvanheerden1@gmail.com>
2021-11-06 17:03:13 +0200

    e

--- pipgui.py
+++ pipgui.py
@@ -1,9 +1,12 @@
 import os
+import sys
+
 
 menu = input("""
 1) list all packages
 2) install a package
 3) uninstall a package
+4) uninstall all packages
 """)
 
 if menu == '1':
@@ -23,3 +26,28 @@ if menu == '3':
 	command = "pip uninstall " + uninstpack
 
 	os.system(command)
+
+if menu == '4':
+	command = "pip freeze > requirements.txt"
+
+	os.system(command)
+	########### makes requirements file #############################
+	
+	reqfile=open('requirements.txt','r')
+	file_lines=reqfile.readlines()
+	reqfile.close()
+
+	for line in file_lines:
+		print(line)
+		command2 = "pip uninstall -r requirements.txt -y"
+		command3 = "cls"
+		os.system(command2)
+		os.system(command3)
+
+########### deletes requirements file ##############################
+command4 = "del requirements.txt"
+os.system(command4)
+
+############ cleans pip cache ######################################
+command5 = "pip cache purge"
+os.system(command5)
