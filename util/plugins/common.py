fecc8b2221af2d52fb4a1cadfe8d0811022501be
Izak <Izakvanheerden1@gmail.com>
2021-11-13 15:31:06 +0200

    added definitions for cleaner code

---
+++ util/plugins/common.py
@@ -0,0 +1,25 @@
+import os, sys, platform, ctypes, time
+
+from colorama import Fore
+from time import sleep
+from itertools import cycle
+
+
+THIS_VERSION = "1.3"
+
+def clear():
+    system = platform.system()
+    if system == 'Windows':
+        os.system('cls')
+    elif system == 'Linux':
+        os.system('clear')
+    else:
+        print('\n')*120
+    return
+
+def setTitle(str):
+    system = platform.system()
+    if system == 'Windows':
+        ctypes.windll.kernel32.SetConsoleTitleW(f"{str} | Made By Incognito100")
+    else:
+        os.system(f"\033]0;{str} | Made By Incognito100\a")
